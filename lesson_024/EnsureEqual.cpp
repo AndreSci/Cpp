c80f0c3b774a80c2c9bb92db2483d8ae05007329
Andrew <41926744+AndreSci@users.noreply.github.com>
2021-02-19 15:45:13 +0300

    Create EnsureEqual.cpp

---
+++ lesson_024/EnsureEqual.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+
+using namespace std;
+
+void EnsureEqual(const string& left, const string& right)
+{
+	if (left != right)
+	{
+		stringstream ss;
+		ss << left << " != " << right;
+		throw runtime_error(ss.str());
+	}
+}
